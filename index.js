6d3160e01d6492ab66a85ea866acfccda1071b51
Solitario97 <a.nurmagambetov97@gmail.com>
2023-03-12 10:15:45 +0600

    some change

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const modal = $.modal();
+
+
+
+
+const modal = $.modal({
+    title: 'Amir modal',
+    closable: true,
+    content: `
+        <h4>Modal is working</h4>
+        <p>lorem ipsum dolor sit.</p>
+    `,
+    width: '400px'
+});
